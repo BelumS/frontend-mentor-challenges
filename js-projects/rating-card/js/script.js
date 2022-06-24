037545f0077f9091943f1edf6f48a2e467255d15
BelumS <beldsahn@protonmail.com>
2022-06-23 21:16:28 -0700

    feat: completed script

--- js-projects/rating-card/js/script.js
+++ js-projects/rating-card/js/script.js
@@ -1,2 +1,33 @@
 'use strict';
 
+//RATING CARD
+const ratingCard = document.querySelector('.card--rating');
+const submitBtn = document.querySelector('.card__btn');
+const ratings = document.querySelectorAll('.card__rating');
+
+// THANK YOU CARD
+const thanksCard = document.querySelector('.card--thanks');
+const thanksMessage = document.querySelector('.card__score-text');
+
+let count = 0;
+
+ratings.forEach(r => {
+    r.addEventListener('click', () => {
+        r.classList.toggle('selected');
+        count++;
+    });
+});
+
+submitBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    ratings.forEach(r => {
+        if(r.classList.contains('selected')) {
+            thanksCard.classList.add('active');
+            ratingCard.classList.remove('active');
+            thanksMessage.textContent = `You selected ${count} out of 5`;
+        } else {
+            return;
+        }
+    });
+});
