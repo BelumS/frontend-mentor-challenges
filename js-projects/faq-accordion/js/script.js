e3a9cb5d67f9145d4b8b6d3bc2bb45baa4899e15
BelumS <beldsahn@protonmail.com>
2022-06-30 15:05:20 -0700

    feat: completed active states

--- js-projects/faq-accordion/js/script.js
+++ js-projects/faq-accordion/js/script.js
@@ -1,49 +1,48 @@
 'use strict';
 
-const cardTop = document.querySelector('.card__top');
-const upArrow = document.querySelector('.card__top-arrow-up');
-const downArrow = document.querySelector('.card__top-arrow-down');
-
-const accordionText = document.querySelector('.card__top-phrase');
-const cardBottom = document.querySelector('.card__bottom');
-const cardBottomText = document.querySelector('.card__bottom-text');
+const cardAccordions = document.querySelectorAll('.card__accordion');
 
 const borderBottomStyle = '1px solid hsl(240, 5%, 91%)';
 
 const add = (element, className = 'selected') => element.classList.add(className);
+const remove = (element, className = 'selected') => element.classList.remove(className);
 const toggle = (element, className = 'selected') => element.classList.toggle(className);
 const contains = (element, className = 'selected') => element.classList.contains(className);
 
-upArrow.addEventListener('click', () => {
-    toggle(upArrow);
-    add(downArrow);
+cardAccordions.forEach(accordion => {
+  const cardTop = accordion.firstElementChild;
+  const accordionText = cardTop.firstElementChild;
+  const arrows = cardTop.lastElementChild;
+  const upArrow = arrows.firstElementChild;
+  const downArrow = arrows.lastElementChild;
+  const cardBottom = accordion.lastElementChild;
 
-    if (contains(downArrow)) {
-        accordionText.style.fontWeight = '600';
-        accordionText.style.color = 'black';
-        cardTop.style.paddingBottom = '3px';
-        cardTop.style.border = 'none';
-        add(cardBottom, 'active');
-
-        cardBottom.style.paddingBottom = '10px';
-        cardBottom.style.borderBottom = borderBottomStyle;
-    } else {
-        return;
-    }
-});
-
-downArrow.addEventListener('click', () => {
+  downArrow.addEventListener('click', () => {
     toggle(downArrow);
     add(upArrow);
 
-    if (contains(upArrow)) {
-        cardTop.style.paddingBottom = '10px';
-        cardTop.style.borderBottom = borderBottomStyle;
-        accordionText.style.fontWeight = '200';
-        accordionText.style.color = 'hsl(240deg, 6%, 50%)';
-        cardBottom.style.border = 'none';
-        toggle(cardBottom, 'active');
-    } else {
-        return;
-    }
+    accordionText.style.fontWeight = '600';
+    accordionText.style.color = 'black';
+
+    cardTop.style.paddingBottom = '3px';
+    cardTop.style.border = 'none';
+
+    add(cardBottom, 'active');
+    cardBottom.style.paddingBottom = '10px';
+    cardBottom.style.borderBottom = borderBottomStyle;
+  });
+
+  upArrow.addEventListener('click', () => {
+      toggle(upArrow);
+      add(downArrow);
+
+      cardTop.style.paddingBottom = '10px';
+      cardTop.style.borderBottom = borderBottomStyle;
+
+      accordionText.style.fontWeight = '200';
+      accordionText.style.color = 'hsl(240deg, 6%, 50%)';
+
+      cardBottom.style.border = 'none';
+      toggle(cardBottom, 'active');
+  });
 });
